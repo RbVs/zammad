d9b3dce5529a70f5ab1818e715c2b0bdb5fb0c8d
Mantas <mantas@idev.lt>
2022-05-13 14:37:58 +0300

    new naming scheme

--- lib/background_services/service/process_scheduled_jobs/retry_limit_reached.rb
+++ lib/background_services/service/process_scheduled_jobs/retry_limit_reached.rb
@@ -1,8 +1,10 @@
 # Copyright (C) 2012-2022 Zammad Foundation, https://zammad-foundation.org/
 
 class BackgroundServices
-  class Scheduled
-    class RetryLimitReached < StandardError
+  module Service
+    class ProcessScheduledJobs
+      class RetryLimitReached < StandardError
+      end
     end
   end
 end
